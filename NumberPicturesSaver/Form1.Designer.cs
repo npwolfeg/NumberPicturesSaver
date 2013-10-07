0f34382cc4ebd12faf933a35c73fb47b134e8be9
wolf <npwolfeg@gmail.com>
2013-10-08 00:17:53 +0400

    drawingThinDigits

---
+++ NumberPicturesSaver/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace NumberPicturesSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(147, 112);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(172, 259);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(253, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 238);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 383);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ NumberPicturesSaver/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NumberPicturesSaver
+{
+    public partial class Form1 : Form
+    {
+        bool canDraw;
+        Bitmap bmp = new Bitmap(100, 100);
+        int[] count = new int[10];
+        string path = @"F:\DigitDB\PictureSaverThin\";
+        Color black = Color.FromArgb(255, 0, 0, 0);
+        Color white = Color.FromArgb(255, 255, 255, 255);
+
+        public Form1()
+        {
+            InitializeComponent();
+            clear();
+            pictureBox1.Image = bmp;
+            using (StreamReader sr = new StreamReader(path+"count.txt"))
+            {
+                for (int i = 0; i < 10; i++)
+                    count[i] = Convert.ToInt32(sr.ReadLine());                    
+            }
+            textBox1.Text = minID().ToString();
+        }
+
+        void clear()
+        {
+            bmp = new Bitmap(100, 100);
+            for (int i = 0; i < 100; i++)
+                for (int j = 0; j < 100; j++)
+                    bmp.SetPixel(i, j, white);
+            pictureBox1.Image = bmp;
+        }
+
+        int minID()
+        {
+            int min = count.Min();
+            int id = count.ToList().IndexOf(min);
+            return id;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            int n = Convert.ToInt32(textBox1.Text);
+            bmp.Save(path+textBox1.Text + count[n].ToString() + ".bmp");
+            count[n]++;
+            using(StreamWriter sw = new StreamWriter(path+"count.txt"))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(count[i].ToString());
+                    listBox1.Items.Add(count[i].ToString());
+                }
+            }
+            clear();
+            n++;
+            if (n == 10)
+                n = 0;
+            textBox1.Text = n.ToString();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                listBox1.Items.Clear();
+                int n = Convert.ToInt32(textBox1.Text);
+                bmp.Save(path + textBox1.Text + count[n].ToString() + ".bmp");
+                count[n]++;
+                using (StreamWriter sw = new StreamWriter(path + "count.txt"))
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        sw.WriteLine(count[i].ToString());
+                        listBox1.Items.Add(count[i].ToString());
+                    }
+                }
+                clear();
+                n++;
+                if (n == 10)
+                    n = 0;
+                textBox1.Text = n.ToString();
+            }
+            else
+            canDraw = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            canDraw = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (canDraw)
+            {
+                for (int i = 0; i < 3; i++)
+                    for (int j = 0; j < 3; j++)
+                        if (e.X + i > -1 && e.X + i < 100 && e.Y + j > -1 && e.Y + j < 100)
+                            bmp.SetPixel(e.X + i, e.Y + j, black);
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+    }
+}
